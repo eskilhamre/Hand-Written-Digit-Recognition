30940dd8f56eef7cb97dc078898b5cda5de5d558
Eskil Hamre Isaksen <eskilusken@gmail.com>
2021-08-11 21:59:07 +0200

    Some minor refactoring

--- js/digit-recognition.js
+++ js/digit-recognition.js
@@ -1,20 +1,18 @@
 //-------------------
 // GLOBAL variables
 //-------------------
-let model;
+const canvasWidth = 150;
+const canvasHeight = 150;
+const canvasStrokeStyle = "white";
+const canvasLineJoin = "round";
+const canvasLineWidth = 10;
+const canvasBackgroundColor = "black";
+const canvasId = "canvas";
 
-var canvasWidth           	= 150;
-var canvasHeight 			= 150;
-var canvasStrokeStyle		= "white";
-var canvasLineJoin			= "round";
-var canvasLineWidth       	= 10;
-var canvasBackgroundColor 	= "black";
-var canvasId              	= "canvas";
-
-var clickX = new Array();
-var clickY = new Array();
-var clickD = new Array();
-var drawing;
+let clickX = [];
+let clickY = [];
+let clickD = [];
+let drawing;
 
 document.getElementById('chart_box').innerHTML = "";
 document.getElementById('chart_box').style.display = "none";
@@ -35,7 +33,11 @@ if(typeof G_vmlCanvasManager != 'undefined') {
 }
 
 ctx = canvas.getContext("2d");
+let model;
+loadModel();
 
+// incremented each time mouse is pressed and moved on canvas
+let mouseMoveWhilePressedCount = 0;   // used to determine when prediction is going to be rerun
 
 //---------------------
 // MOUSE DOWN function
@@ -69,16 +71,19 @@ canvas.addEventListener("touchstart", function (e) {
 
 }, false);
 
+
 //---------------------
 // MOUSE MOVE function
 //---------------------
 $("#canvas").mousemove(function(e) {
-	if(drawing) {
+	if (drawing) {
 		var rect = canvas.getBoundingClientRect();
 		var mouseX = e.clientX- rect.left;;
 		var mouseY = e.clientY- rect.top;
 		addUserGesture(mouseX, mouseY, true);
 		drawOnCanvas();
+		console.log(`mouse move nr: ${mouseMoveWhilePressedCount}`)
+		mouseMoveWhilePressedCount++;
 	}
 });
 
@@ -172,9 +177,9 @@ function drawOnCanvas() {
 //------------------------
 $("#clear-button").click(async function () {
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-	clickX = new Array();
-	clickY = new Array();
-	clickD = new Array();
+	clickX = [];
+	clickY = [];
+	clickD = [];
 	$(".prediction-text").empty();
 	$("#result_box").addClass('d-none');
 });
@@ -187,14 +192,12 @@ async function loadModel() {
 
   // clear the model variable
   model = undefined;
-  
+
   // load the model using a HTTPS request (where you have stored your model files)
   model = await tf.loadLayersModel("models/model.json");
-  
+
   console.log("model loaded..");
 }
-
-loadModel();
 
 //-----------------------------------------------
 // preprocess the canvas
@@ -207,7 +210,7 @@ function preprocessCanvas(image) {
 		.expandDims(2)
 		.expandDims()
 		.toFloat();
-	console.log(tensor.shape);
+	console.log(`canvas preprocessed, resulting tensor shape: ${tensor.shape}`);
 	return tensor.div(255.0);
 }
 
@@ -215,32 +218,29 @@ function preprocessCanvas(image) {
 // predict function 
 //--------------------------------------------
 $("#predict-button").click(async function () {
-    // get image data from canvas
-	var imageData = canvas.toDataURL();
-
 	// preprocess canvas
 	let tensor = preprocessCanvas(canvas);
 
 	// make predictions on the preprocessed image tensor
-	let predictions = await model.predict(tensor).data();
+	let raw_predictions = await model.predict(tensor).data();
 
 	// get the model's prediction results
-	let results = Array.from(predictions);
+	let predictions = Array.from(raw_predictions);
 
 	// display the predictions in chart
 	$("#result_box").removeClass('d-none');
-	displayChart(results);
-	displayLabel(results);
+	displayChart(predictions);
+	displayLabel(predictions);
 
-	console.log(results);
+	console.log(`results from prediction: ${predictions}`);
 });
 
 //------------------------------
 // Chart to display predictions
 //------------------------------
-var chart = "";
-var firstTime = 0;
-function loadChart(label, data, modelSelected) {
+let chart = "";
+
+function loadChart(predictions) {
 	var ctx = document.getElementById('chart_box').getContext('2d');
 	chart = new Chart(ctx, {
 	    // The type of chart we want to create
@@ -248,12 +248,12 @@ function loadChart(label, data, modelSelected) {
 
 	    // The data for our dataset
 	    data: {
-	        labels: label,
+	        labels: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
 	        datasets: [{
-	            label: modelSelected + " prediction",
+	            label: "Model predictions",
 	            backgroundColor: '#f50057',
 	            borderColor: 'rgb(255, 99, 132)',
-	            data: data,
+	            data: predictions,
 	        }]
 	    },
 
@@ -266,31 +266,29 @@ function loadChart(label, data, modelSelected) {
 // display chart with updated
 // drawing from canvas
 //----------------------------
-function displayChart(data) {
-	var select_model  = document.getElementById("select_model");
-  	var select_option = "CNN";
+let firstTime = 0;
 
-	label = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-	if (firstTime == 0) {
-		loadChart(label, data, select_option);
+function displayChart(predictions) {
+	if (firstTime === 0) {
+		loadChart(predictions);
 		firstTime = 1;
 	} else {
 		chart.destroy();
-		loadChart(label, data, select_option);
+		loadChart(predictions);
 	}
 	document.getElementById('chart_box').style.display = "block";
 }
 
-function displayLabel(data) {
-	var max = data[0];
+function displayLabel(predictions) {
+	var max = predictions[0];
     var maxIndex = 0;
 
-    for (var i = 1; i < data.length; i++) {
-        if (data[i] > max) {
+    for (var i = 1; i < predictions.length; i++) {
+        if (predictions[i] > max) {
             maxIndex = i;
-            max = data[i];
+            max = predictions[i];
         }
     }
-	$(".prediction-text").html("Predicting you draw <b>"+maxIndex+"</b> with <b>"+Math.trunc( max*100 )+"%</b> confidence")
+	$(".prediction-text").html("Predicting you drew <b>"+maxIndex+"</b> with <b>"+Math.trunc( max*100 )+"%</b> confidence")
 }
 
